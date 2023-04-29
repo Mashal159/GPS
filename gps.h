34983341b0d5fac27fcf589faab83faa8eddec71
Mashal159 <127619882+Mashal159@users.noreply.github.com>
2023-04-29 16:17:25 +0200

    Update gps.h

--- gps.h
+++ gps.h
@@ -1,4 +1,4 @@
-int gpsValidate(char *gpsString){
+int gpsValidate(char *gpsString)
 float read_GPS_data(char* gpsString)
 double distance(double lat1, double lon1, double lat2, double lon2)
 double distance_inMeter(double KM_dis)

20eed1ae43006592f883efa3c5bf5b83df6b0607
Mashal159 <127619882+Mashal159@users.noreply.github.com>
2023-04-30 20:15:37 +0200

    Update gps.h

--- gps.h
+++ gps.h
@@ -1,4 +1,4 @@
 int gpsValidate(char *gpsString)
-float read_GPS_data(char* gpsString)
+void read_GPS_data(char* gpsString, float* latitude, float* longitude, float* time, float* speed)
 double distance(double lat1, double lon1, double lat2, double lon2)
 double distance_inMeter(double KM_dis)
